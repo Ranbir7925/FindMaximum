69b292c1a1e85eb9b1f60a6e61781802168367d6
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-31 12:25:10 +0530

    ADDED:Function to check maximum variable value

--- src/main/java/com/bridgelabz/findmaximum/MaximumVariable.java
+++ src/main/java/com/bridgelabz/findmaximum/MaximumVariable.java
@@ -2,8 +2,13 @@ package com.bridgelabz.findmaximum;
 public class MaximumVariable
 {
 
-    public String welcomeMessage(String message)
+    public int maximumValue(Integer firstNumber,Integer secondNumber,Integer thirdNUmber)
     {
-        return "Welcome";
+        if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNUmber) > 0)
+            return firstNumber;
+        else if (secondNumber.compareTo(thirdNUmber) > 0 && secondNumber.compareTo(firstNumber) > 0)
+            return secondNumber;
+        else
+            return thirdNUmber;
     }
 }
