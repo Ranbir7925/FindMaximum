87ff1817ed7f5066bb2645e86551b96b11f26660
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-31 13:00:10 +0530

    ADDED:Test method to get maximum number placed at first position

--- src/test/java/com/bridgelabz/findmaximum/TestMaximumVariable.java
+++ src/test/java/com/bridgelabz/findmaximum/TestMaximumVariable.java
@@ -1,15 +1,23 @@
 package com.bridgelabz.findmaximum;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestMaximumVariable
 {
+   MaximumVariable maximumVariable=null;
+   @Before
+   public void createObjectOf_MaximumVariableClass()
+   {
+        maximumVariable = new MaximumVariable();
+   }
+
     @Test
-    public void testWelcomeMessage()
+    public void givenMaximumNumberAt_FirstPosition_ShouldReturnSameNumber()
     {
-        MaximumVariable maximumVariable = new MaximumVariable();
-        String message = maximumVariable.welcomeMessage("Welcome to find maximum value");
-        Assert.assertEquals("Welcome",message);
+        Integer maximum = maximumVariable.maximumValue(99,55,44);
+        Assert.assertEquals((Integer)99,maximum);
     }
 }
