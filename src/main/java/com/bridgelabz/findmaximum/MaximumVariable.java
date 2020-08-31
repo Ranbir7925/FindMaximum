85c7663080bd91e4ea8c0499d3bb7cc60c5e1c27
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-31 14:28:22 +0530

    REFACTORED:Method to work with all type data i.e Generic type

--- src/main/java/com/bridgelabz/findmaximum/MaximumVariable.java
+++ src/main/java/com/bridgelabz/findmaximum/MaximumVariable.java
@@ -2,53 +2,20 @@ package com.bridgelabz.findmaximum;
 public class MaximumVariable
 {
     /**
-     *METHOD TO FIND OUT MAXIMUM AMONG 3 INTEGERS
-     * @param firstNumber takes 1st Integer Value
-     * @param secondNumber takes 2nd Integer Value
-     * @param thirdNUmber takes 3rd Integer Value
-     * @return maximum Integer value
-     */
-    public Integer maximumIntegerValue(Integer firstNumber,Integer secondNumber,Integer thirdNUmber)
-    {
-        if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNUmber) > 0)
-            return firstNumber;
-        else if (secondNumber.compareTo(thirdNUmber) > 0 && secondNumber.compareTo(firstNumber) > 0)
-            return secondNumber;
-        else
-            return thirdNUmber;
-
-    }
-
-    /**
      *
-     * @param firstNumber takes 1st Float Value
-     * @param secondNumber takes 2nd Float Value
-     * @param thirdNUmber takes 3rd Float Value
-     * @return maximum Float value
+     * @param firstValue takes 1st Value
+     * @param secondValue takes 2nd Value
+     * @param thirdValue takes 3rd Value
+     * @return maximum value
      */
-    public Float maximumFloatValue(Float firstNumber, Float secondNumber, Float thirdNUmber)
+    public static < T extends Comparable > T maximumValue(T firstValue, T secondValue, T thirdValue)
     {
-        if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNUmber) > 0)
-            return firstNumber;
-        else if (secondNumber.compareTo(thirdNUmber) > 0 && secondNumber.compareTo(firstNumber) > 0)
-            return secondNumber;
+        if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0)
+            return firstValue;
+        else if (secondValue.compareTo(thirdValue) > 0 && secondValue.compareTo(firstValue) > 0)
+            return secondValue;
         else
-            return thirdNUmber;
+            return thirdValue;
     }
-    /**
-     *
-     * @param firstString takes 1st String Value
-     * @param secondString takes 2nd String Value
-     * @param thirdString takes 3rd String Value
-     * @return maximum String value
-     */
-    public String maximumStringValue(String firstString, String secondString, String thirdString)
-    {
-        if (firstString.compareTo(secondString) > 0 && firstString.compareTo(thirdString) > 0)
-            return firstString;
-        else if (secondString.compareTo(thirdString) > 0 && secondString.compareTo(firstString) > 0)
-            return secondString;
-        else
-            return thirdString;
-    }
+
 }
--- src/test/java/com/bridgelabz/findmaximum/TestMaximumVariable.java
+++ src/test/java/com/bridgelabz/findmaximum/TestMaximumVariable.java
@@ -16,63 +16,63 @@ public class TestMaximumVariable
     @Test
     public void givenMaximumIntegerValueAt_FirstPosition_ShouldReturnSameNumber()
     {
-        Integer maximum = maximumVariable.maximumIntegerValue(99,55,44);
+        Integer maximum = maximumVariable.maximumValue(99,55,44);
         Assert.assertEquals((Integer)99,maximum);
     }
 
     @Test
     public void givenMaximumIntegerValueAt_SecondPosition_ShouldReturnSameNumber()
     {
-        Integer maximum = maximumVariable.maximumIntegerValue(55,99,69);
+        Integer maximum = maximumVariable.maximumValue(55,99,69);
         Assert.assertEquals((Integer)99,maximum);
     }
 
     @Test
     public void givenMaximumIntegerValueAt_ThirdPosition_ShouldReturnSameNumber()
     {
-        Integer maximum = maximumVariable.maximumIntegerValue(55,69,99);
+        Integer maximum = maximumVariable.maximumValue(55,69,99);
         Assert.assertEquals((Integer) 99,maximum);
     }
 
     @Test
     public void givenMaximumFloatValueAt_FirstPosition_ShouldReturnSameValue()
     {
-        Float maximum = maximumVariable.maximumFloatValue(9.1f,4.5f,8.8f);
+        Float maximum = maximumVariable.maximumValue(9.1f,4.5f,8.8f);
         Assert.assertEquals((Float)9.1f,maximum);
     }
 
     @Test
     public void givenMaximumFloatValueAt_SecondPosition_ShouldReturnSameValue()
     {
-        Float maximum = maximumVariable.maximumFloatValue(4.5f,9.1f,8.8f);
+        Float maximum = maximumVariable.maximumValue(4.5f,9.1f,8.8f);
         Assert.assertEquals((Float)9.1f,maximum);
     }
 
     @Test
     public void givenMaximumFloatValueAt_ThirdPosition_ShouldReturnSameValue()
     {
-        Float maximum = maximumVariable.maximumFloatValue(4.5f,8.8f,9.1f);
+        Float maximum = maximumVariable.maximumValue(4.5f,8.8f,9.1f);
         Assert.assertEquals((Float)9.1f,maximum);
     }
 
     @Test
     public void givenMaximumStringValueAt_FirstPosition_ShouldReturnSameValue()
     {
-        String maximum = maximumVariable.maximumStringValue("Peach","Apple","Banana");
+        String maximum = maximumVariable.maximumValue("Peach","Apple","Banana");
         Assert.assertEquals("Peach" ,maximum);
     }
 
     @Test
     public void givenMaximumStringValueAt_SecondPosition_ShouldReturnSameValue()
     {
-        String maximum = maximumVariable.maximumStringValue("Apple","Peach","Banana");
+        String maximum = maximumVariable.maximumValue("Apple","Peach","Banana");
         Assert.assertEquals("Peach" ,maximum);
     }
 
     @Test
     public void givenMaximumStringValueAt_ThirdPosition_ShouldReturnSameValue()
     {
-        String maximum = maximumVariable.maximumStringValue("Apple","Banana","Peach");
+        String maximum = maximumVariable.maximumValue("Apple","Banana","Peach");
         Assert.assertEquals("Peach" ,maximum);
     }
 }
