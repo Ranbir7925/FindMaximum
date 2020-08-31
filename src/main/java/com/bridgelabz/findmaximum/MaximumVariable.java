fdaa0b5b704c13c86c997f38bb6ef9cfb8b8ecdd
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-31 13:23:38 +0530

    REFACTORED:Proper documentation added

--- src/main/java/com/bridgelabz/findmaximum/MaximumVariable.java
+++ src/main/java/com/bridgelabz/findmaximum/MaximumVariable.java
@@ -1,7 +1,13 @@
 package com.bridgelabz.findmaximum;
 public class MaximumVariable
 {
-
+    /**
+     *METHOD TO FIND OUT MAXIMUM AMONG 3 INTEGERS
+     * @param firstNumber takes 1st Integer Value
+     * @param secondNumber takes 2nd Integer Value
+     * @param thirdNUmber takes 3rd Integer Value
+     * @return maximum Integer value
+     */
     public Integer maximumIntegerValue(Integer firstNumber,Integer secondNumber,Integer thirdNUmber)
     {
         if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNUmber) > 0)
@@ -13,6 +19,13 @@ public class MaximumVariable
 
     }
 
+    /**
+     *
+     * @param firstNumber takes 1st Float Value
+     * @param secondNumber takes 2nd Float Value
+     * @param thirdNUmber takes 3rd Float Value
+     * @return maximum Float value
+     */
     public Float maximumFloatValue(Float firstNumber, Float secondNumber, Float thirdNUmber)
     {
         if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNUmber) > 0)
